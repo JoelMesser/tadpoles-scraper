5ea565a0017138a02f1bb2ab7b5f8a5b05e6bbc8
Joel Messer <joel.messer@visionistinc.com>
2019-07-01 18:48:13 -0400

    Adding functionality

--- tadpoles.py
+++ tadpoles.py
@@ -11,11 +11,13 @@ class TadpoleScraper():
     def __init__(self, cookie, uid, endTime=None):
         self.startTime = endTime
         self.endTime = None
+
         self.children = {}
+        self.attachments = []
         self.scraper = GatedScraper(cookie=args.cookie, uid=args.uid)
         self.scraper.add_job('/'.join([BASE_URL, 'parents']), self.parentScrape)
 
-    def parentScrape(response):
+    def parentScrape(self, response):
         htmlResponse = response.read().decode("utf-8")
         tmpSplit = htmlResponse.splitlines()
         tadpolesParams = None
@@ -26,17 +28,54 @@ class TadpoleScraper():
         tadpolesJson = json.loads(tadpolesParams)
         if self.startTime == None:
             self.startTime = tadpolesJson['first_event_time']
+
         self.endTime = tadpolesJson['last_event_time']
+
         for kid in tadpolesJson['children']:
             self.children[kid['key']] = kid['display_name']
 
-        self.scraper.add_job(EVENTS % { start_time: self.startTime, end_time: self.endTime, num_events: 300 }, self.parseEvents)
+        self.scraper.add_job(EVENTS.format(start_time=self.startTime, end_time=self.endTime, num_events=300), self.parseEvents)
+
+    def processAttachments(self):
+        print(str(len(self.attachments)) + " attachments to parse")
 
     def parseEvents(self, response):
         txtResponse = response.read().decode("utf-8")
         jsonResponse = json.loads(txtResponse)
 
-        jsonResponse
+        if len(jsonResponse['events']) == 0:
+            self.processAttachments()
+            return
+
+
+        last_time = 0
+        for singleEvent in jsonResponse['events']:
+            last_time = max(last_time, singleEvent['create_time'])
+            # if the event has an attachment, push it
+            if 'attachments' in singleEvent:
+                for singleAttach in singleEvent['attachments']:
+                    toPush = {}
+                    toPush['attachment'] = singleAttach
+                    toPush['child'] = singleEvent['parent_member_display']
+                    toPush['create_time'] = singleEvent['create_time']
+                    toPush['comment'] = None
+                    self.attachments.append(toPush)
+            
+            # If the event has entries, check them
+            if 'entries' in singleEvent:
+                for singleEntry in singleEvent['entries']:
+                    if 'attachment' in singleEntry:
+                        toPush = {}
+                        toPush['attachment'] =  singleEntry['attachment']['key'],
+                        toPush['child'] = singleEvent['parent_member_display'],
+                        toPush['create_time'] = singleEvent['create_time'],
+                        if 'note' in singleEntry:
+                            toPush['comment'] = singleEntry['note']
+                        self.attachments.append(toPush) 
+
+        self.scraper.add_job(EVENTS.format(start_time=last_time, end_time=self.endTime, num_events=300), self.parseEvents)
+
+                #Push event onto stack.
 
 BASE_URL = 'https://www.tadpoles.com'
 
@@ -54,9 +93,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    scraper = GatedScraper(cookie=args.cookie, uid=args.uid)
-
-    scraper.add_job('/'.join([BASE_URL, 'parents']), parentScrape)
+    scraper = TadpoleScraper(cookie=args.cookie, uid=args.uid)
 
     try:
         main_loop()
